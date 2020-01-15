9360c85cf4b14000071644395a6b4ecfe8d9f997
ShaneBeuerman <shanebeuerman@gmail.com>
2020-01-15 13:19:51 -0700

    Fixed scoring system
    The scoring works for all 13 types of dice rolls. Added comments too.

--- src/casino/Yahtzee.java
+++ src/casino/Yahtzee.java
@@ -8,6 +8,7 @@ public class Yahtzee extends Game {
     public static void play(){
         int choice;
         rollDice();
+        displayDice();
         System.out.println("Would you like to reroll? 1 for yes. Anything else for no.");
         choice = user.nextInt();
         switch(choice){
@@ -23,12 +24,19 @@ public class Yahtzee extends Game {
         }
     }
     
+    /*
+        Displays the value of each die.
+    */
     public static void displayDice(){
+        Arrays.sort(dice);
         for(int i = 0; i < dice.length; i++){
-            System.out.println("Dice " + i + " is " + dice[i]);
+            System.out.println("Dice " + (i+1) + " is " + dice[i]);
         }
     }
     
+    /*
+        Rerolls whichever die you have selected.
+    */
     public static void reroll(){
         Random reRoll = new Random();
         System.out.println("Which die would you like to reroll?");
@@ -36,25 +44,28 @@ public class Yahtzee extends Game {
         switch(choice){
             case 1:
                 dice[0] = reRoll.nextInt(6) + 1;
+                displayDice();
                 System.out.println("Dice 1 is " + dice[0] + " now.");
                 break;
             case 2:
                 dice[1] = reRoll.nextInt(6) + 1;
+                displayDice();
                 System.out.println("Dice 2 is " + dice[1] + " now.");
                 break;
             case 3:
                 dice[2] = reRoll.nextInt(6) + 1;
+                displayDice();
                 System.out.println("Dice 3 is " + dice[2] + " now.");
                 break;
             case 4:
                 dice[3] = reRoll.nextInt(6) + 1;
+                displayDice();
                 System.out.println("Dice 4 is " + dice[3] + " now.");
-                System.out.println("");
                 break;
             case 5:
                 dice[4] = reRoll.nextInt(6) + 1;
+                displayDice();
                 System.out.println("Dice 5 is " + dice[4] + " now.");
-                System.out.println("");
                 break;
             default:
                 System.out.println("Not an option. No reroll for you.");
@@ -62,63 +73,132 @@ public class Yahtzee extends Game {
         }
     }
     
+    /*
+        Rolls each of the six dice.
+    */
     public static void rollDice(){
         Random roll = new Random();
         for(int i = 0; i < dice.length; i++){
             dice[i] = roll.nextInt(6) + 1;
-            System.out.println("Dice " + (i+1) + " has " + dice[i]);
         }
+        Arrays.sort(dice);
     }
     
+    /*
+        Evaluate the score of all of your dice.
+
+    */
     public static void evaluateScore(){
-        Arrays.sort(dice);    
-        int score = 0;
-        score = chance();
-        System.out.println("Your score is " + score);
+        Arrays.sort(dice);
+        System.out.println("Aces is " + aces());
+        System.out.println("Twos is " + twos());
+        System.out.println("Threes is " + threes());
+        System.out.println("Fours is " + fours());
+        System.out.println("Fives is " + fives());
+        System.out.println("Sixes is " + sixes());
+        System.out.println("Yahtzee is " + yahtzee());
+        System.out.println("Small straight is " + smallStraight());
+        System.out.println("Large straight is " + largeStraight());
+        System.out.println("Three of a Kind is " + threeOfAKind());
+        System.out.println("Four of a Kind is " + fourOfAKind());
+        System.out.println("Full House is " + fullHouse());
+        System.out.println("Chane is " + chance());
+        //int score = 0;
+        //score = chance();
+        //System.out.println("Your score is " + score);
     }
     
+    /*
+        Determine your yahtzee score. If you have a yahtzee,
+        you receive 50 points. Otherwise it is 0.
+    */
     public static int yahtzee(){
         int score = 0;
-        
+        if(dice[0] == dice[4]){
+            score = 50;
+        }
         System.out.println("Yahtzee score is " + score);
         return 0;
     }
     
+    /*
+        Determine if you have a smallStraight. If you have a
+        small straight, then you receive 30 points.
+    */
     public static int smallStraight(){
         int score = 0;
-        
+        if(dice[0] == 1 && dice[1] == 2 && dice[2] == 3 && dice[3] == 4 && dice[4] == 5){
+            score = 30;
+        }
         System.out.println("Small straight score is " + score);
         return 0;
     }
     
+    /*
+        Determine if you have a largeStraight. If you have a 
+        large straight, then you receive 40 points
+    */
     public static int largeStraight(){
         int score = 0;
-        
+        if(dice[0] == 2 && dice[1] == 3 && dice[2] == 4 && dice[3] == 5 && dice[4] == 6){
+            score = 40;
+        }
         System.out.println("Large straight score is " + score);
         return 0;
     }
     
+    /*
+        Checks if you have three dice with the same value. If
+        you do have three of a kind, then your score returned
+        is the sum of your dice.
+    */
     public static int threeOfAKind(){
         int score = 0;
-        
+        if(dice[0] == dice[2] || dice[1] == dice[3] || dice[2] == dice[4]){
+            for(int i = 0; i < dice.length; i++){
+                score += dice[i];
+            }
+        }
+             
         System.out.println("Three of a kind score is " + score);
-        return 0;
+        return score;
     }
     
+    /*
+        Checks if you have four dice with the same value. If you
+        do have four of a kind, then your score returned is the
+        sum of your dice.
+    */
     public static int fourOfAKind(){
         int score = 0;
-        
+        if(dice[0] == dice[3] || dice[1] == dice[4]){
+            for(int i = 0; i < dice.length; i++){
+                score += dice[i];
+            }
+        }
         System.out.println("Four of a kind score is " + score);
-        return 0;
+        return score;
     }
     
+    /*
+        Similar to that of a poker hand. If you have three dice
+        with the same value and a pair of dice with same value,
+        then you get a score of 25 points.
+    */
     public static int fullHouse(){
         int score = 0;
+        if((dice[0] == dice[1] && dice[2] == dice[4]) ||
+           (dice[0] == dice[2]) && dice[3] == dice[4]){
+            score = 25;
+        }
        
         System.out.println("Full House score is " + score);
-        return 0;
+        return score;
     }
     
+    /*
+        Returns the sum of the five dice.
+    */
     public static int chance(){
         int score = 0;
         for(int i = 0; i < dice.length; i++){
@@ -128,6 +208,10 @@ public class Yahtzee extends Game {
         return score;
     }
     
+    /*
+        Returns the sum of all the ones that you
+        have rolled.
+    */
     public static int aces(){
         int score = 0;
         for(int i = 0; i < dice.length; i++){
@@ -139,6 +223,10 @@ public class Yahtzee extends Game {
         return score;
     }
     
+    /*
+        Returns the sum of all the twos that you
+        have rolled.
+    */
     public static int twos(){
         int score = 0;
         for(int i = 0; i < dice.length; i++){
@@ -150,6 +238,10 @@ public class Yahtzee extends Game {
         return score;
     }
     
+    /*
+        Returns the sum of all the threes that you
+        have rolled.
+    */
     public static int threes(){
         int score = 0;
         for(int i = 0; i < dice.length; i++){
@@ -161,6 +253,10 @@ public class Yahtzee extends Game {
         return score;
     }
     
+    /*
+        Returns the sum of all the fours that you
+        have rolled.
+    */
     public static int fours(){
         int score = 0;
         for(int i = 0; i < dice.length; i++){
@@ -172,6 +268,10 @@ public class Yahtzee extends Game {
         return score;
     }
     
+    /*
+        Returns the sum of all the fives that you
+        have rolled.
+    */
     public static int fives(){
         int score = 0;
         for(int i = 0; i < dice.length; i++){
@@ -183,6 +283,10 @@ public class Yahtzee extends Game {
         return score;
     }
     
+    /*
+        Returns the sum of all the sixes that you
+        have rolled.
+    */
     public static int sixes(){
         int score = 0;
         for(int i = 0; i < dice.length; i++){
